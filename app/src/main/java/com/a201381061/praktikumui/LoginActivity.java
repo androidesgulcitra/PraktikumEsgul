e8a0a7ae59141d22c5d6e09bff0d4359cd6d04d9
Handrianto Gunawan <handriantog@gmail.com>
2018-04-21 11:16:15 +0700

    Update

--- app/src/main/java/com/a201381061/praktikumui/LoginActivity.java
+++ app/src/main/java/com/a201381061/praktikumui/LoginActivity.java
@@ -1,13 +1,54 @@
 package com.a201381061.praktikumui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity{
+
+    EditText etUsername;
+    EditText etPassword;
+    Button buttonLogin;
+    TextView tvJudul;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        casting();
     }
+
+    private void casting(){
+        //CASTING
+        etUsername = (EditText)findViewById(R.id.username);
+        etPassword = (EditText)findViewById(R.id.password);
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser(){
+        String isiUsername = etUsername.getText().toString();
+        String isiPassword = etPassword.getText().toString();
+
+        if(isiUsername.equals("ADMIN")&&isiPassword.equals("12345")){
+            //LOGIN BERHASIL
+            Toast.makeText(LoginActivity.this,"Login Berhasil",Toast.LENGTH_SHORT).show();
+        }else{
+            //LOGIN GAGAL
+            Toast.makeText(LoginActivity.this,"Login Gagal",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/a201381061/praktikumui/MainActivity.java
+++ app/src/main/java/com/a201381061/praktikumui/MainActivity.java
@@ -2,12 +2,53 @@ package com.a201381061.praktikumui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        casting();
     }
+
+
+    private void casting(){
+        etUsername = (EditText)findViewById(R.id.username);
+        etPassword = (EditText)findViewById(R.id.password);
+        buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser(){
+        String isiUsername = etUsername.getText().toString();
+        String isPassword = etPassword.getText().toString();
+
+        if(isiUsername.equals("ADMIN")&&isPassword.equals("12345")){
+            //INI KETIKA USER BERHASIL LOGIN
+            Toast.makeText(this,"Login Berhasil",Toast.LENGTH_SHORT).show();
+        }else{
+            //INI KETIKA USER GAGAL LOGIN
+
+            Toast.makeText(this,"Login Gagal",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
+
 }
