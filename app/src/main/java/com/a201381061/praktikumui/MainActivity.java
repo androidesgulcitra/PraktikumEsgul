86037a3a899079d2095f933268b19234fd41e442
Handrianto Gunawan <handriantog@gmail.com>
2018-05-26 11:20:42 +0700

    Login with volley

--- app/src/main/java/com/a201381061/praktikumui/MainActivity.java
+++ app/src/main/java/com/a201381061/praktikumui/MainActivity.java
@@ -1,12 +1,24 @@
 package com.a201381061.praktikumui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
         casting();
     }
 
-
     private void casting(){
         etUsername = (EditText)findViewById(R.id.username);
         etPassword = (EditText)findViewById(R.id.password);
@@ -29,12 +40,55 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginUser();
+                loginOnline();
             }
         });
     }
 
-    private void loginUser(){
+    private void loginOnline(){
+        final String isiUsername = etUsername.getText().toString();
+        final String isiPassword = etPassword.getText().toString();
+
+        String url = "http://tiw.web.id/api/mobileapi.php";
+        JsonObjectRequest loginUser = new JsonObjectRequest(Request.Method.POST,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //INI BERHASIL
+                Log.d("Response",response.toString());
+                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //GAGAL
+                Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("UserName",isiUsername);
+                params.put("PassWord",isiPassword);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("appid","fasilkom");
+                return params;
+            }
+        };
+
+        MyApplication.getInstance().addToRequestQueue(loginUser,"ASDF");
+
+    }
+
+
+
+    private void loginOffline(){
         String isiUsername = etUsername.getText().toString();
         String isPassword = etPassword.getText().toString();
 
@@ -55,8 +109,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Login Gagal",Toast.LENGTH_SHORT).show();
         }
     }
-
-
-
 
 }
---
+++ app/src/main/java/com/a201381061/praktikumui/MyApplication.java
@@ -0,0 +1,46 @@
+package com.a201381061.praktikumui;
+
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MyApplication extends Application {
+
+    private RequestQueue requestQueue;
+
+    private static MyApplication mInstance;
+
+    public void onCreate()
+    {
+        super.onCreate();
+        mInstance=this;
+    }
+
+    public static synchronized MyApplication getInstance()
+    {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue()
+    {
+        if (requestQueue==null)
+            requestQueue= Volley.newRequestQueue(getApplicationContext());
+
+        return requestQueue;
+    }
+
+    public void addToRequestQueue(Request request, String tag)
+    {
+        request.setTag(tag);
+        getRequestQueue().add(request);
+
+    }
+
+    public void cancelAllRequests(String tag)
+    {
+        getRequestQueue().cancelAll(tag);
+    }
+}
